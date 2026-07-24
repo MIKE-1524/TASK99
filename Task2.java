ee5a2e874c01f9db9657d1e51b061a47ba9899b1
MIKE-1524 <pimentelmike45@gmail.com>
2026-07-24 07:31:21 +0000

    Task2

---
+++ Task2.java
@@ -0,0 +1,18 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+        
+        byte b = 3;
+        int i = 11;
+        short s = 0;
+        char c = 'H';
+        char c1 = 'w';
+        boolean bol = true;
+        float f = 2.0f;
+        
+        String output = c + "" + b + "" + i + "" + s + " " + c1 + s + "rld " + f + " " + bol;
+
+        System.out.println(output);
+    
+    }
+}
